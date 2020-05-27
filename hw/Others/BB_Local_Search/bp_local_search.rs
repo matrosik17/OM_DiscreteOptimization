e999fb31beb327e0c7c5d903b4f8fb689a233a6b
Matrosov Sergey <matrosik14@gmail.com>
2020-05-28 00:09:13 +0300

    add xoshiro256ss rng and rename file

--- hw/Others/BB_Local_Search/bp_local_search.rs
+++ hw/Others/BB_Local_Search/bp_local_search.rs
@@ -157,3 +157,55 @@ mod io {
     }
 
 }
+
+mod rng {
+
+    pub struct Xoshiro256ss {
+        state: [u64; 4],
+    }
+
+    impl Xoshiro256ss {
+        pub fn new(seed: u64) -> Self {
+            let mut state = [0; 4];
+            let mut split_mix = SplitMix64 { state: seed };
+            state[0] = split_mix.rand();
+            state[1] = split_mix.rand();
+            state[2] = split_mix.rand();
+            state[3] = split_mix.rand();
+            Self { state }
+        }
+
+        fn rol64(x: u64, k: i64) ->u64 {
+            (x << k) | (x >> (64 - k))
+        }
+
+        pub fn rand(&mut self) -> u64 {
+            let result = Self::rol64(self.state[1] * 5, 7) * 9;
+            let t = self.state[1] << 17;
+
+            self.state[2] ^= self.state[0];
+            self.state[3] ^= self.state[1];
+            self.state[1] ^= self.state[2];
+            self.state[0] ^= self.state[3];
+
+            self.state[2] ^= t;
+            self.state[3] = Self::rol64(self.state[3], 45);
+
+            return result;
+        }
+    }
+
+    struct SplitMix64 {
+        state: u64,
+    }
+
+    impl SplitMix64 {
+        fn rand(&mut self) -> u64 {
+            let mut result = self.state;
+            self.state = result + 0x9E3779B97f4A7C15;
+            result = (result ^ (result >> 30)) * 0xBF58476D1CE4E5B9;
+            result = (result ^ (result >> 27)) * 0x94D049BB133111EB;
+            return result ^ (result >> 31);
+        }
+    }
+}
