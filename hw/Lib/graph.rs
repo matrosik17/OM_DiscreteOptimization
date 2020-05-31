b4a82b4f3bbeb9804132f8a43cd1556c29f8594a
Matrosov Sergey <matrosik14@gmail.com>
2020-05-31 23:29:25 +0300

    save graph lib

--- hw/Lib/graph.rs
+++ hw/Lib/graph.rs
@@ -1,125 +1,78 @@
-//! Basic graph module without explicit support for deletion.
-//!
-//! # Panics
-//!
-//! All methods will panic if given an out-of-bounds element index.
-pub mod connectivity;
-pub mod flow;
-mod util;
+mod graph {
+    use std::ops::Index;
 
-/// Represents a union of disjoint sets. Each set's elements are arranged in a
-/// tree, whose root is the set's representative.
-pub struct DisjointSets {
-    parent: Vec<usize>,
-}
+    pub struct Graph<N, E>
+    {
+        nodes: Vec<N>,
+        edges: Vec<Vec<(usize, E)>>,
+    }
 
-impl DisjointSets {
-    /// Initializes disjoint sets containing one element each.
-    pub fn new(size: usize) -> Self {
-        Self {
-            parent: (0..size).collect(),
+    impl<N, E> Graph<N, E>
+    where E: Copy,
+    {
+        pub fn new() -> Self {
+            Self {
+                nodes: Vec::new(),
+                edges: Vec::new(),
+            }
+        }
+
+        pub fn from_nodes(nodes: Vec<N>) -> Self {
+            let n_nodes = nodes.len();
+            Self {
+                nodes,
+                edges: vec![Vec::new(); n_nodes],
+            }
+        }
+
+        pub fn size(&self) -> (usize, usize) {
+            let n_nodes = self.nodes.len();
+            let n_edges = self.edges.iter().map(|neigh| neigh.len()).sum();
+            (n_nodes, n_edges)
+        }
+
+        pub fn neigh(&self, node_idx: usize) -> &[(usize, E)] {
+            &self.edges[node_idx]
+        }
+
+        pub fn neigh_mut(&mut self, node_idx: usize) -> &mut Vec<(usize, E)> {
+            &mut self.edges[node_idx]
+        }
+
+        pub fn add_node(&mut self, node: N) -> usize {
+            self.nodes.push(node);
+            self.edges.push(Vec::new());
+            self.nodes.len() - 1
+        }
+
+        pub fn add_edge(&mut self, node1: usize, node2: usize, weight: E) {
+            self.edges[node1].push((node2, weight));
+        }
+
+        pub fn add_edge_undirected(&mut self, node1: usize, node2: usize, weight: E) {
+            self.edges[node1].push((node2, weight));
+            self.edges[node2].push((node1, weight));
         }
     }
 
-    /// Finds the set's representative. Do path compression along the way to make
-    /// future queries faster.
-    pub fn find(&mut self, u: usize) -> usize {
-        let pu = self.parent[u];
-        if pu != u {
-            self.parent[u] = self.find(pu);
-        }
-        self.parent[u]
-    }
+    // graph[i] - значение в вершине
+    impl<N, E> Index<usize> for Graph<N, E> {
+        type Output = N;
 
-    /// Merges the sets containing u and v into a single set containing their
-    /// union. Returns true if u and v were previously in different sets.
-    pub fn merge(&mut self, u: usize, v: usize) -> bool {
-        let (pu, pv) = (self.find(u), self.find(v));
-        self.parent[pu] = pv;
-        pu != pv
-    }
-}
-
-/// A compact graph representation. Edges are numbered in order of insertion.
-/// Each adjacency list consists of all edges pointing out from a given vertex.
-pub struct Graph {
-    /// Maps a vertex id to the first edge in its adjacency list.
-    first: Vec<Option<usize>>,
-    /// Maps an edge id to the next edge in the same adjacency list.
-    next: Vec<Option<usize>>,
-    /// Maps an edge id to the vertex that it points to.
-    endp: Vec<usize>,
-}
-
-impl Graph {
-    /// Initializes a graph with vmax vertices and no edges. To reduce
-    /// unnecessary allocations, emax_hint should be close to the number of
-    /// edges that will be inserted.
-    pub fn new(vmax: usize, emax_hint: usize) -> Self {
-        Self {
-            first: vec![None; vmax],
-            next: Vec::with_capacity(emax_hint),
-            endp: Vec::with_capacity(emax_hint),
+        fn index(&self, node_idx: usize) -> &Self::Output {
+            &self.nodes[node_idx]
         }
     }
 
-    /// Returns the number of vertices.
-    pub fn num_v(&self) -> usize {
-        self.first.len()
-    }
+    // graph[(i, j)] - значение ребра (i, j)
+    impl<N, E> Index<(usize, usize)> for Graph<N, E> {
+        type Output = E;
 
-    /// Returns the number of edges, double-counting undirected edges.
-    pub fn num_e(&self) -> usize {
-        self.endp.len()
-    }
-
-    /// Adds a directed edge from u to v.
-    pub fn add_edge(&mut self, u: usize, v: usize) {
-        self.next.push(self.first[u]);
-        self.first[u] = Some(self.num_e());
-        self.endp.push(v);
-    }
-
-    /// An undirected edge is two directed edges. If edges are added only via
-    /// this funcion, the reverse of any edge e can be found at e^1.
-    pub fn add_undirected_edge(&mut self, u: usize, v: usize) {
-        self.add_edge(u, v);
-        self.add_edge(v, u);
-    }
-
-    /// If we think of each even-numbered vertex as a variable, and its
-    /// odd-numbered successor as its negation, then we can build the
-    /// implication graph corresponding to any 2-CNF formula.
-    /// Note that u||v == !u -> v == !v -> u.
-    pub fn add_two_sat_clause(&mut self, u: usize, v: usize) {
-        self.add_edge(u ^ 1, v);
-        self.add_edge(v ^ 1, u);
-    }
-
-    /// Gets vertex u's adjacency list.
-    pub fn adj_list(&self, u: usize) -> AdjListIterator {
-        AdjListIterator {
-            graph: self,
-            next_e: self.first[u],
+        fn index(&self, edge_idx: (usize, usize)) -> &Self::Output {
+            let (node1, node2) = edge_idx;
+            let edge = self.edges[node1].iter().find(|(idx, _)| *idx == node2);
+            let (_, weight) = edge.expect("Edge doesn't exist");
+            weight
         }
-    }
-}
-
-/// An iterator for convenient adjacency list traversal.
-pub struct AdjListIterator<'a> {
-    graph: &'a Graph,
-    next_e: Option<usize>,
-}
-
-impl<'a> Iterator for AdjListIterator<'a> {
-    type Item = (usize, usize);
-
-    /// Produces an outgoing edge and vertex.
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next_e.map(|e| {
-            let v = self.graph.endp[e];
-            self.next_e = self.graph.next[e];
-            (e, v)
-        })
     }
 }
